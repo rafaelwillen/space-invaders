7f81dd62fef3b09742a66b3c388a282fda5bbf7e
Angleu <angleuzua99@gmail.com>
2021-10-17 21:12:31 +0100

    nave of player

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const nave = new NavePlayer();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: '#fff' } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const player = nave.createNavePlayer();
+scene.add( player );
 
 camera.position.z = 3;
+nave.movePlayer(player);
 
-renderer.render(scene, camera);
+function animate() {
+    renderer.render( scene, camera );
+}
+animate();
+ 
