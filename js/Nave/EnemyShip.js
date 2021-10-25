645cdb0a6c2a9c15c582fe41bfa2e0278192a66f
Rafael Willen <rafaelpadre20@gmail.com>
2021-10-25 10:12:46 +0100

    Adicionada movimentação suave na nave inimiga

--- js/Nave/EnemyShip.js
+++ js/Nave/EnemyShip.js
@@ -1,13 +1,12 @@
 class EnemyShip {
   constructor() {
-    this.speed = 0.2;
+    this.step = 0;
     // Cria um grupo para juntar várias partes da nave
     this.shipObject = new THREE.Group();
     this.shipObject.name = "nave-inimiga";
     // Cria uma simples caixa verde 10x10x10
     const basicBox = this.buildBasicBox();
     this.shipObject.add(basicBox);
-    this.shipObject.userData.direction = "left";
   }
 
   buildBasicBox() {
@@ -19,19 +18,8 @@ class EnemyShip {
   }
 
   move(threshold) {
-    const shipPositionX = this.shipObject.position.x;
-    let newDirection = this.shipObject.userData.direction;
-    // Se passar o threshold, a nave muda de direção
-    if (shipPositionX >= threshold) newDirection = "left";
-    else if (shipPositionX <= threshold * -1) newDirection = "right";
-    switch (newDirection) {
-      case "left":
-        this.shipObject.position.x -= this.speed;
-        break;
-      case "right":
-        this.shipObject.position.x += this.speed;
-        break;
-    }
-    this.shipObject.userData.direction = newDirection;
+    this.step += 0.02;
+    this.shipObject.position.x = (threshold + 1) * Math.sin(this.step);
+    console.log(this.shipObject.position.x);
   }
 }
